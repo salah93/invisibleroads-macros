d947177ab705c4249d6c563477eabdbda14292eb
Roy Hyunjin Han <rhh@crosscompute.com>
2015-07-15 10:48:48 -0400

    Add remove_punctuation

--- invisibleroads_macros/text.py
+++ invisibleroads_macros/text.py
@@ -2,6 +2,7 @@ import re
 
 
 WHITESPACE_PATTERN = re.compile(r'\s+', re.MULTILINE)
+PUNCTUATION_PATTERN = re.compile(r'[^a-zA-Z\s]+')
 
 
 def parse_words(x):
@@ -14,3 +15,7 @@ def strip_whitespace(string):
 
 def compact_whitespace(string):
     return WHITESPACE_PATTERN.sub(' ', string).strip()
+
+
+def remove_punctuation(string):
+    return compact_whitespace(PUNCTUATION_PATTERN.sub(' ', string))
