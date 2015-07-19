2cc036288bbc3b2602c561e457f60d2c85c9f4e3
Roy Hyunjin Han <rhh@crosscompute.com>
2015-07-19 18:59:36 -0400

    Restore consistency

--- invisibleroads_macros/log.py
+++ invisibleroads_macros/log.py
@@ -12,8 +12,8 @@ def format_nested_dictionary(d, prefix=''):
             lines.append(format_nested_dictionary(value, left_hand_side + '.'))
         elif isinstance(value, basestring) and '\n' in value:
             value_lines = value.splitlines()
-            lines.append(left_hand_side + ' = ' + value_lines[0])
-            for line in value_lines[1:]:
+            lines.append(left_hand_side + ' =')
+            for line in value_lines:
                 lines.append('  ' + line)
         else:
             lines.append(left_hand_side + ' = ' + str(value))
