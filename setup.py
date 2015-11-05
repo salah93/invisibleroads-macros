3767a8071dbb6e89c4afcc5d54e53fc74005d71b
Roy Hyunjin Han <rhh@crosscompute.com>
2015-11-05 13:40:53 -0500

    Prepare for PyPI

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
     'README.rst', 'CHANGES.rst'])
 setup(
     name='invisibleroads-macros',
-    version='0.6.0',
+    version='0.6.1',
     description='Shortcut functions',
     long_description=DESCRIPTION,
     classifiers=[
